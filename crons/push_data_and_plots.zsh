d7ba34a4ae41dc0e23d57e02d06ff0697e5d154a
Adam Kit <adam.kit@vtt.fi>
2025-04-21 14:01:53 +0300

    Update for the cron job to update data

---
+++ crons/push_data_and_plots.zsh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+# File to push the data and plots via git 
+WD=$HOME/dev/ghouse 
+cd $WD
+git add data/ 
+git add datavisualisation/figures
+git add datacollection/
+
+TODAY=$(date +%Y-%m-%d)
+
+git status
+echo pushing data to $TODAY
+git commit -m "updaing data $TODAY"
+git push origin main 
+echo pushed data to $TODAY
